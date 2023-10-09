20d212f51a2c63a5fa6e2debcdf4ff9feb96de68
SakuniSenanayake-12133959 <146144842+SakuniSenanayake-12133959@users.noreply.github.com>
2023-10-09 23:07:34 +1100

    Create PatientReportView.fxml, AppointmentReportView.fxml, FinancailReportView.fxml and all controller java files to these mentioned .fxml files and jasper report for patients details along with jasper report libraries.

--- src/main/java/Utils/DbConnectionManager.java
+++ src/main/java/Utils/DbConnectionManager.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Utils;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -18,119 +19,152 @@ import model.User;
  * @author Chamali
  */
 public class DbConnectionManager {
+
     private static DbConnectionManager connectionManager = null;
     private Connection connection;
-    
+
     private String dbName = "hospital_db";
     private String user = "root";
-    private String password = "asdf1234";
+    private String password = "Sakuni19931017";//asdf1234
     private String url = "jdbc:mysql://localhost";
-        
-    private DbConnectionManager() {
+
+    public DbConnectionManager() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             connection = DriverManager.getConnection(url, user, password);
-            
+
             String createQuery = "CREATE DATABASE IF NOT EXISTS " + dbName;
             String useQuery = "USE " + dbName;
-            
+
             Statement statement = connection.createStatement();
             statement.addBatch(createQuery);
             statement.addBatch(useQuery);
 
             statement.executeBatch();
-            
+
             System.out.println("Database connection successful");
-            
+
         } catch (Exception e) {
             System.out.println(e);
         }
     }
-    
+
     public static DbConnectionManager shared() {
-        if (connectionManager == null)
+        if (connectionManager == null) {
             connectionManager = new DbConnectionManager();
-          
+        }
+
         return connectionManager;
     }
-    
+
     public void createTablesIfNeeded() {
         createPatientTableIfNeeded();
         createUserTableIfNeeded();
+        createPatientReportTableIfNeeded();
     }
-    
+
     public Connection getConnection() {
         return this.connection;
     }
-    
+
     private boolean doesTableExist(String tableName) {
         String query = String.format("SHOW TABLES LIKE '%s'", tableName);
-        
+
         try {
             Statement statement = connection.createStatement();
             statement.execute(query);
-            
+
             return statement.getResultSet().next();
         } catch (Exception e) {
             e.printStackTrace();
             return false;
         }
     }
-    
+
     private void createUserTableIfNeeded() {
         if (!doesTableExist(TableName.user)) {
             String createQuery = "CREATE TABLE " + TableName.user + " (\n"
-                + "userId INT NOT NULL AUTO_INCREMENT, \n"
-                + "name varchar(255), \n" 
-                + "phone varchar(255), \n"
-                + "email varchar(255), \n"
-                + "username varchar(255) NOT NULL, \n"
-                + "password varchar(255) NOT NULL, \n"
-                + "role varchar(255) NOT NULL, \n"
-                + "PRIMARY KEY (userId));";
-            
+                    + "userId INT NOT NULL AUTO_INCREMENT, \n"
+                    + "name varchar(255), \n"
+                    + "phone varchar(255), \n"
+                    + "email varchar(255), \n"
+                    + "username varchar(255) NOT NULL, \n"
+                    + "password varchar(255) NOT NULL, \n"
+                    + "role varchar(255) NOT NULL, \n"
+                    + "PRIMARY KEY (userId));";
+
             String incrementQuery = "ALTER TABLE " + TableName.user + " AUTO_INCREMENT = 001";
-            
+
             try {
                 Statement statement = connection.createStatement();
                 statement.addBatch(createQuery);
                 //statement.addBatch(insertQuery);
-                
+
                 statement.executeBatch();
             } catch (SQLException e) {
                 System.out.println(e.getMessage());
             }
         }
     }
-    
-    
-  
+
     private void createPatientTableIfNeeded() {
         if (!doesTableExist(TableName.patient)) {
             String createQuery = "CREATE TABLE " + TableName.patient + " (\n"
-                + "patientId INT NOT NULL AUTO_INCREMENT, \n"
-                + "fName varchar(255) NOT NULL, \n"
-                + "lName varchar(255) NOT NULL, \n"
-                + "address varchar(255) NOT NULL, \n"
-                + "mobile varchar(255) NOT NULL, \n"
-                + "email varchar(255) NOT NULL, \n"
-                + "gender varchar(255) NOT NULL, \n"
-                + "insuaranceID varchar(255) NOT NULL, \n"
-                + "dob date NOT NULL, \n"
-                + "PRIMARY KEY (patientId));";
-            
+                    + "patientId INT NOT NULL AUTO_INCREMENT, \n"
+                    + "fName varchar(255) NOT NULL, \n"
+                    + "lName varchar(255) NOT NULL, \n"
+                    + "address varchar(255) NOT NULL, \n"
+                    + "mobile varchar(255) NOT NULL, \n"
+                    + "email varchar(255) NOT NULL, \n"
+                    + "gender varchar(255) NOT NULL, \n"
+                    + "insuaranceID varchar(255) NOT NULL, \n"
+                    + "dob date NOT NULL, \n"
+                    + "PRIMARY KEY (patientId));";
+
             String incrementQuery = "ALTER TABLE " + TableName.patient + " AUTO_INCREMENT = 1001";
-            
-            String fkConstraint = "ALTER TABLE " + TableName.login + " \n" +
-                "ADD CONSTRAINT FK_Patient_patientId FOREIGN KEY (patientId) \n" +
-                "REFERENCES "+ TableName.patient + "(patientId);";
-        
+
+            String fkConstraint = "ALTER TABLE " + TableName.login + " \n"
+                    + "ADD CONSTRAINT FK_Patient_patientId FOREIGN KEY (patientId) \n"
+                    + "REFERENCES " + TableName.patient + "(patientId);";
+
             try {
                 Statement statement = connection.createStatement();
                 statement.addBatch(createQuery);
                 statement.addBatch(incrementQuery);
                 //statement.addBatch(fkConstraint);
-                
+
+                statement.executeBatch();
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private void createPatientReportTableIfNeeded() {
+        if (!doesTableExist(TableName.patient_report)) {
+            String createQuery = "CREATE TABLE " + TableName.patient_report + " (\n"
+                    + "patientReportId INT NOT NULL AUTO_INCREMENT, \n"
+                    + "fName varchar(255) NOT NULL, \n"
+                    + "lName varchar(255) NOT NULL, \n"
+                    + "month varchar(255) NOT NULL, \n"
+                    + "disease varchar(255) NOT NULL, \n"
+                    + "age INT NOT NULL, \n"
+                    + "phone varchar(255) NOT NULL, \n"
+                    + "email varchar(255) NOT NULL, \n"
+                    + "address varchar(255) NOT NULL, \n"
+                    + "dateCreated varchar(255) NOT NULL, \n"
+                    + "insuranceId varchar(255) NOT NULL, \n"
+                    + "patientId INT NOT NULL, \n"
+                    + "PRIMARY KEY (patientReportId));";
+
+            String incrementQuery = "ALTER TABLE " + TableName.patient_report + " AUTO_INCREMENT = 2001";
+
+            try {
+                Statement statement = connection.createStatement();
+                statement.addBatch(createQuery);
+                statement.addBatch(incrementQuery);
+                //statement.addBatch(fkConstraint);
+
                 statement.executeBatch();
             } catch (SQLException e) {
                 System.out.println(e.getMessage());
